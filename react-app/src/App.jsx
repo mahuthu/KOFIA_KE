b232f1fd4adf1abcd9edf6c4912c63f475ed8632
mahuthu <mahuthup@gmail.com>
2024-02-11 21:44:59 +0300

    version3

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,10 +1,30 @@
-//import ProductList from './pages/ProductList';
-//import Home from './pages/home';
-//import Product from './pages/Product';
-//import Login from "./pages/Login";
+import ProductList from './pages/ProductList';
+import Home from './pages/home';
+import Product from './pages/Product';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Cart from './pages/Cart';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
 
 const App = () => {
-  return<Cart/>;
-}
+  const user = true;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+      </Routes>
+    </Router>
+  );
+};
 export default App;
--- react-app/src/components/Products.jsx
+++ react-app/src/components/Products.jsx
@@ -10,7 +10,50 @@ const Container = styled.div`
     justify-content: space-between;
     `;
 
-const Products = () => {
+const Products = ({cat, filters, sort}) => {
+
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  useEffect(() => {
+    cat ? setFilteredProducts(popularProducts.filter(item => item.cat === cat)) :
+    setFilteredProducts(popularProducts);
+  }, [cat])
+
+  useEffect(() => {
+    cat && setFilteredProducts(popularProducts.filter(item => item.cat === cat))
+  }, [cat])
+
+  useEffect(() => {
+    setFilteredProducts(() => {
+      if(sort === "newest") {
+        return filteredProducts.sort((a,b) => a.id - b.id);
+      }
+      if(sort === "asc") {
+        return filteredProducts.sort((a,b) => a.price - b.price);
+      }
+      if(sort === "desc") {
+        return filteredProducts.sort((a,b) => b.price - a.price);
+      }
+      return filteredProducts;
+    })
+  }
+  , [sort, cat])
+
+  useEffect(() => {
+    setFilteredProducts(() => {
+      return popularProducts.filter(item => {
+        for (const key in filters) {
+          if (item[key] !== filters[key]) {
+            return false;
+          }
+        }
+        return true;
+      })
+    })
+  }
+  , [filters])
+
+  
   return (
     <Container>
         {popularProducts.map((item) => (
